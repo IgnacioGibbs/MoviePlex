9e304b4a3173c5441c4008dcefc0f3b6e37ea6c7
Ignacio Gibbs <ignacio.gibbs@gmail.com>
2024-02-02 12:10:26 -0300

    adding voteAverage to search

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,5 +1,6 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:movieplex/config/helpers/human_formats.dart';
 import 'package:movieplex/domain/entities/movie.dart';
 
 typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
@@ -62,20 +63,61 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
 
           return ListView.builder(
               itemCount: movies.length,
-              itemBuilder: (context, index) {
-                final movie = movies[index];
-
-                return ListTile(
-                  leading: FadeInLeft(
-                      child: Image.network(
-                          movie.posterPath ??=
-                              'https://www.freeiconspng.com/uploads/no-image-icon-4.png',
-                          width: 50.0,
-                          fit: BoxFit.cover)),
-                  title: FadeInRight(child: Text(movie.title)),
-                  onTap: () => close(context, movie),
-                );
-              });
+              itemBuilder: (context, index) =>
+                  _MovieItem(movie: movies[index]));
         });
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieItem({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = Theme.of(context).textTheme;
+    final size = MediaQuery.of(context).size;
+
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: Row(children: [
+          SizedBox(
+            width: size.width * 0.2,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Image.network(movie.posterPath,
+                  fit: BoxFit.cover,
+                  loadingBuilder: (context, child, loadingProgress) =>
+                      FadeIn(child: child)),
+            ),
+          ),
+          const SizedBox(width: 10),
+          SizedBox(
+            width: size.width * 0.7,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(movie.title, style: textStyle.titleMedium),
+                (movie.overview.length > 100 && movie.overview.isNotEmpty)
+                    ? Text('${movie.overview.substring(0, 100)}...',
+                        style: textStyle.titleSmall)
+                    : Text(movie.overview, style: textStyle.titleSmall),
+                Row(
+                  children: [
+                    const Icon(Icons.star_half_rounded, color: Colors.amber),
+                    const SizedBox(width: 5),
+                    Text(
+                        HumanFormats.number(movie.voteAverage,
+                            decimalDigits: 1),
+                        style: textStyle.bodySmall!.copyWith(
+                            color: Colors.amber.shade600,
+                            fontWeight: FontWeight.bold)),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ]));
   }
 }
